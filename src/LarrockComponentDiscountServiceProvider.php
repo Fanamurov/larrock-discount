40bf94dafaf3caf2a3b596b8887cd88db4f9b971
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-28 13:23:00 +1000

    Fix migrations

--- src/LarrockComponentDiscountServiceProvider.php
+++ src/LarrockComponentDiscountServiceProvider.php
@@ -33,23 +33,21 @@ class LarrockComponentDiscountServiceProvider extends ServiceProvider
         $timestamp = date('Y_m_d_His', time());
         $migrations = [];
         if ( !class_exists('CreateDiscountTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_create_discount_table.php' => database_path('migrations/'.$timestamp.'_create_discount_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_create_discount_table.php'] = database_path('migrations/'.$timestamp.'_create_discount_table.php');
         }
         if ( !class_exists('UpdateCartDiscountsTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_update_cart_table.php' => database_path('migrations/'.$timestamp.'_update_cart_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_update_cart_table.php'] = database_path('migrations/'.$timestamp.'_update_cart_table.php')];
         }
         if ( !class_exists('UpdateCategoryDiscountsTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_update_category_table.php' => database_path('migrations/'.$timestamp.'_update_category_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_update_category_table.php'] = database_path('migrations/'.$timestamp.'_update_category_table.php');
         }
         if ( !class_exists('AddForeignKeysToCartDiscountTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_cart_table.php' => database_path('migrations/'.$timestamp.'_add_foreign_keys_to_cart_discounts_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_cart_table.php'] = database_path('migrations/'.$timestamp.'_add_foreign_keys_to_cart_discounts_table.php');
         }
         if ( !class_exists('AddForeignKeysToCategoryDiscountTable')){
-            $migrations = [__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_table.php' => database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_discounts_table.php')];
+            $migrations[__DIR__.'/../database/migrations/0000_00_00_000000_add_foreign_keys_to_category_table.php'] = database_path('migrations/'.$timestamp.'_add_foreign_keys_to_category_discounts_table.php');
         }
 
-        $this->publishes([
-            $migrations
-        ], 'migrations');
+        $this->publishes($migrations, 'migrations');
     }
 }
