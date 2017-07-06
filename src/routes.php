a0c40c64414ed5a04b4f83f95a1d6a582db68dae
Alexandr Fanamurov <fanamura@gmail.com>
2017-07-06 13:37:58 +1000

    Fix

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Larrock\ComponentDiscount\AdminDiscountController;
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
     Route::resource('discount', AdminDiscountController::class, ['names' => [
         'index' => 'admin.discount.index',
         'show' => 'admin.discount.show',
