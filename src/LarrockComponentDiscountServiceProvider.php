ff00e66e7aa93a5090e68b26a74b4d7ca40fa13a
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-16 14:26:51 +1000

    update

--- src/LarrockComponentDiscountServiceProvider.php
+++ src/LarrockComponentDiscountServiceProvider.php
@@ -16,6 +16,7 @@ class LarrockComponentDiscountServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
         $this->loadViewsFrom(__DIR__.'/views', 'larrock');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/vendor/larrock')
@@ -32,26 +33,5 @@ class LarrockComponentDiscountServiceProvider extends ServiceProvider
         $this->app->make(DiscountComponent::class);
 
         $this->app['router']->aliasMiddleware('DiscountsShare', DiscountsShare::class);
-
-        $timestamp = date('Y_m_d_His', time());
-        $timestamp_after = date('Y_m_d_His', time()+10);
-        $migrations = [];
-        if ( !class_exists('CreateDiscountTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_create_discount_table.php'] = database_path('migrations/'.$timestamp.'_create_discount_table.php');
-        }
-        if ( !class_exists('UpdateCartDiscountsTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_update_cart_discounts_table.php'] = database_path('migrations/'.$timestamp.'_update_cart_discounts_table.php');
-        }
-        if ( !class_exists('UpdateCategoryDiscountsTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_update_category_discounts_table.php'] = database_path('migrations/'.$timestamp.'_update_category_discounts_table.php');
-        }
-        if ( !class_exists('AddForeignKeysToCartDiscountTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_add_foreign_keys_to_cart_discount_table.php'] = database_path('migrations/'.$timestamp_after.'_add_foreign_keys_to_cart_discount_table.php');
-        }
-        if ( !class_exists('AddForeignKeysToCategoryDiscountTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_add_foreign_keys_to_category_discount_table.php'] = database_path('migrations/'.$timestamp_after.'_add_foreign_keys_to_category_discount_table.php');
-        }
-
-        $this->publishes($migrations, 'migrations');
     }
 }
