672e16cdc8d1e7a06321defa0aa95f0c967c2a02
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-31 14:58:02 +1000

    +searchSiteAdmin Middleware

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Larrock\ComponentDiscount\AdminDiscountController;
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData', 'SiteSearchAdmin']], function(){
     Route::resource('discount', AdminDiscountController::class, ['names' => [
         'index' => 'admin.discount.index',
         'show' => 'admin.discount.show',
