e183e65aa4f01268e5c76f0b86508c867a06ff52
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-22 17:01:03 +1000

    rewrite routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,7 @@
 <?php
 
-use Larrock\ComponentDiscount\AdminDiscountController;
-
 Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData', 'SiteSearchAdmin']], function(){
-    Route::resource('discount', AdminDiscountController::class, ['names' => [
+    Route::resource('discount', 'Larrock\ComponentDiscount\AdminDiscountController', ['names' => [
         'index' => 'admin.discount.index',
         'show' => 'admin.discount.show',
     ]]);
