5fa588a4b7c47fb682997ffc6da1215d8a6082ae
Alexandr Fanamurov <fanamura@gmail.com>
2017-11-23 12:42:49 +1000

    update component

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+Route::post('/ajax/checkKuponDiscount', 'Larrock\ComponentDiscount\DiscountController@checkKuponDiscount')->name('checkKuponDiscount');
+
 Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData', 'SiteSearchAdmin']], function(){
     Route::resource('discount', 'Larrock\ComponentDiscount\AdminDiscountController', ['names' => [
         'index' => 'admin.discount.index',
