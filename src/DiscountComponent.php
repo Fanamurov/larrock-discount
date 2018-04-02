d5f8cec7105b40e841d519e714b2c226d52a8741
Alexandr Fanamurov <fanamurov@ya.ru>
2018-04-02 14:31:20 +1000

    component config now using setRow method

--- src/DiscountComponent.php
+++ src/DiscountComponent.php
@@ -23,48 +23,45 @@ class DiscountComponent extends Component
     protected function addRows()
     {
         $row = new FormInput('title', 'Название скидки');
-        $this->rows['title'] = $row->setValid('max:255|required')->setTypo()->setFillable();
+        $this->setRow($row->setValid('max:255|required')->setTypo()->setFillable());
 
         $row = new FormTextarea('description', 'Описание скидки');
-        $this->rows['description'] = $row->setTypo()->setFillable();
+        $this->setRow($row->setTypo()->setFillable());
 
         $row = new FormSelect('type', 'Тип скидки');
-        $this->rows['type'] = $row->setValid('max:255|required')->setDefaultValue('Скидка в корзине')
+        $this->setRow($row->setValid('max:255|required')->setDefaultValue('Скидка в корзине')
             ->setOptions(['Скидка в корзине', 'Накопительная скидка', 'Купон', 'Скидка для параметра'])
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable();
+            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable());
 
         $row = new FormInput('word', 'Слово-активатор скидки');
-        $this->rows['word'] = $row->setValid('max:255')
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setInTableAdmin()->setFillable();
+        $this->setRow($row->setValid('max:255')
+            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setInTableAdmin()->setFillable());
 
         $row = new FormInput('cost_min', 'Минимальная сумма для активации');
-        $this->rows['cost_min'] = $row->setValid('integer')
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable();
+        $this->setRow($row->setValid('integer')->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable());
 
         $row = new FormInput('cost_max', 'Максимальная сумма для активации');
-        $this->rows['cost_max'] = $row->setValid('integer')
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable();
+        $this->setRow($row->setValid('integer')->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable());
 
         $row = new FormInput('percent', 'Скидка к сумме в процентах');
-        $this->rows['percent'] = $row->setValid('max:100|integer')
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setInTableAdmin()->setFillable();
+        $this->setRow($row->setValid('max:100|integer')
+            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setInTableAdmin()->setFillable());
 
         $row = new FormInput('num', 'Скидка к сумме в абс. величине');
-        $this->rows['num'] = $row->setValid('integer')
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setInTableAdmin()->setFillable();
+        $this->setRow($row->setValid('integer')->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')
+            ->setInTableAdmin()->setFillable());
 
         $row = new FormInput('d_count', 'Сколько раз может быть использован');
-        $this->rows['d_count'] = $row->setValid('integer')
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setInTableAdmin()
-            ->setFillable()->setDefaultValue(9999999);
+        $this->setRow($row->setValid('integer')->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')
+            ->setInTableAdmin()->setFillable()->setDefaultValue(9999999));
 
         $row = new FormDate('date_start', 'Дата начала акции');
-        $this->rows['date_start'] = $row->setDefaultValue(date('Y-m-d H:i:s'))
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable();
+        $this->setRow($row->setDefaultValue(date('Y-m-d H:i:s'))
+            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable());
 
         $row = new FormDate('date_end', 'Дата окончания акции');
-        $this->rows['date_end'] = $row->setDefaultValue(date('Y-m-d H:i:s'))
-            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable();
+        $this->setRow($row->setDefaultValue(date('Y-m-d H:i:s'))
+            ->setCssClassGroup('uk-width-1-2 uk-width-1-3@m')->setFillable());
 
         return $this;
     }
